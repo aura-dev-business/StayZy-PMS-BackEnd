9d248607f78e98d8062d212594bdfd9bb25c3601
Muhammed Rashad PTP <113594567+rashadptp@users.noreply.github.com>
2025-08-22 12:36:33 +0400

    Update StayzyBackendApplicationTests.java

--- src/test/java/com/stayzy/StayzyBackendApplicationTests.java
+++ src/test/java/com/stayzy/StayzyBackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.stayzy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // <-- use H2
 class StayzyBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
